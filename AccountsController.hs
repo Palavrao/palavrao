1c7199dea686c866cbeea748e167a945f86dbb7f
samuel.lucas <samuel.matos@wildlifestudios.com>
2024-03-13 10:01:21 -0300

    feat: adding get matches and accounts logic

--- AccountsController.hs
+++ AccountsController.hs
@@ -8,7 +8,7 @@ import Data.Aeson
 import Utils as UT
 
 data Account = Account {
-    name :: String,
+    accName :: String,
     score :: Int
 } deriving (Show, Generic)
 
@@ -17,3 +17,14 @@ instance FromJSON Account
 
 createAcc :: Account -> IO()
 createAcc acc = UT.updateJsonFile acc "data/accounts.json"
+
+getAccByName :: String -> IO (Maybe Account)
+getAccByName targetName = do
+    accs <- UT.readJsonFile "data/accounts.json"
+    return $ _getAccByName accs targetName
+
+_getAccByName :: [Account] -> String -> Maybe Account
+_getAccByName [] _ = Nothing
+_getAccByName (acc:acct) targetName 
+    | accName acc == targetName = Just acc
+    | otherwise = _getAccByName acct targetName
--- MatchesController.hs
+++ MatchesController.hs
@@ -8,6 +8,7 @@ import Data.Aeson
 import Utils as UT
 
 data Match = Match {
+    matchName :: String,
     p1Name :: String,
     p1Score :: Int,
     p2Name :: String,
@@ -19,3 +20,14 @@ instance FromJSON Match
 
 createMatch :: Match -> IO()
 createMatch match = UT.updateJsonFile match "data/matches.json"
+
+getMatchByName :: String -> IO (Maybe Match)
+getMatchByName targetName = do
+    matches <- UT.readJsonFile "data/matches.json"
+    return $ _getMatchByName matches targetName
+
+_getMatchByName :: [Match] -> String -> Maybe Match
+_getMatchByName [] _ = Nothing
+_getMatchByName (match:matcht) targetName 
+    | matchName match == targetName = Just match
+    | otherwise = _getMatchByName matcht targetName
