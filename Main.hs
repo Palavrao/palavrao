eb23c899fdc45af0e4903c70227e73afc008843c
Helena Home <mhsatyro@gmail.com>
2024-03-14 07:38:54 -0300

    Prepara Tabuleiro preliminar

--- Main.hs
+++ Main.hs
@@ -5,10 +5,18 @@ import MatchesController
 import GameLoop
 
 
-
 main :: IO ()
 main = do
-    verTabuleiro
-    createAcc Account {name="fulano", score=0}
-    createAcc Account {name="beltrano", score=0}
-    createMatch Match {p1Name="fulano", p1Score=0, p2Name="beltrano", p2Score=0}
+    
+    createAcc Account {accName="fulano", score=0}
+    createAcc Account {accName="beltrano", score=0}
+    let m = Match {matchName="match1", p1Name="fulano", p1Score=0, p2Name="beltrano", p2Score=0}
+    createMatch m
+
+    let t = startTabuleiro m
+    verTabuleiro t
+    
+    --mapM (mapM replacements) (curTiles t) 
+    putStr ""
+
+    
