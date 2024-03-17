bdf8ddf272e72b6e1663fb68bddb133f5582a1d8
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2024-03-17 10:56:31 -0300

    feat: adding board to match

--- MatchesController.hs
+++ MatchesController.hs
@@ -4,11 +4,14 @@ module MatchesController where
 
 import GHC.Generics
 import Data.Aeson
+import BoardController;
+import AccountsController
 
 import Utils as UT
 
 data Match = Match {
     matchName :: String,
+    board :: Board,
     p1Name :: String,
     p1Score :: Int,
     p2Name :: String,
@@ -20,6 +23,12 @@ instance FromJSON Match
 
 saveMatch :: Match -> IO()
 saveMatch match = UT.incJsonFile match "data/matches.json"
+
+createMatch :: String -> Account -> Account -> IO(Match)
+createMatch name p1 p2 = do
+    let match = Match {matchName = name, board = startBoard, p1Name = accName p1, p1Score = 0, p2Name = accName p2, p2Score = 0}
+    saveMatch match
+    return match
 
 getMatchByName :: String -> IO (Maybe Match)
 getMatchByName targetName = do
