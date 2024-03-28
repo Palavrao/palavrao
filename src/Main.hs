ba75ea699ed0a98f244df161645844c5169b5ac9
samuel.lucas <samuel.matos@wildlifestudios.com>
2024-03-28 09:28:04 -0300

    docs: adding main doc

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,14 @@
+{- 
+    Jogo de palavras baseado em Scrabble, com sistema de contas e partidas
+    
+    Produzido por: 
+    - Samuel Lucas (https://github.com/SamuelLucasVM)
+    - Helena Sátyro (https://github.com/helenasatyro)
+    - Eliane Tamara (https://github.com/elianetamara)
+    - Paulo Ricardo (https://github.com/paulorpn)
+    - Rayanne Macêdo (https://github.com/raiaiaia)
+-} 
+
 module Main where
 
 import Utils.Utils as UT
