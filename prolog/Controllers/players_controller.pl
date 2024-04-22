313f936fd3ab16d3cca9e0d6fc8dc230f2b9065f
samuel.lucas <samuel.matos@wildlifestudios.com>
2024-04-22 10:04:01 -0300

    refactor: changing update queries

--- prolog/Controllers/players_controller.pl
+++ prolog/Controllers/players_controller.pl
@@ -9,6 +9,7 @@ create_player(MatchName , PlayerName) :-
 
 get_player(MatchName, PlayerName, player(MatchName, PlayerName, PlayerLetters, PlayerScore)).
 
+
 get_player_letters(MatchName, PlayerName, PlayerLetters) :- 
     player(MatchName, PlayerName, PlayerLetters, _).
 
@@ -21,8 +22,7 @@ inc_score(MatchName, PlayerName, IncScore) :-
     players_path(PlayersPath),
 
     get_player(MatchName, PlayerName, Player),
-    get_player_letters(MatchName, PlayerName, PlayerLetters),
-    get_player_score(MatchName, PlayerName, PlayerScore),
+    player(MatchName, PlayerName, PlayerLetters, PlayerScore),
 
     NewScore is PlayerScore + IncScore,
 
@@ -33,7 +33,7 @@ update_letters(MatchName, PlayerName, NewLetters) :-
     players_path(PlayersPath),
 
     get_player(MatchName, PlayerName, Player),
-    get_player_score(MatchName, PlayerName, PlayerScore),
+    player(MatchName, PlayerName, _, PlayerScore),
 
     update_fact_file(PlayersPath, Player, player(MatchName, PlayerName, NewLetters, PlayerScore)).
 
@@ -42,9 +42,8 @@ add_letters(MatchName, PlayerName, NewLetters) :-
     players_path(PlayersPath),
 
     get_player(MatchName, PlayerName, Player),
-    get_player_letters(MatchName, PlayerName, PlayerLetters),
-    get_player_score(MatchName, PlayerName, PlayerScore),
+    player(MatchName, PlayerName, PlayerLetters, PlayerScore),
 
     append(PlayerLetters, NewLetters, UpdatedLetters),
 
-    update_fact_file(PlayersPath, Player, player(MatchName, PlayerName, NewLetters, UpdatedLetters)).
+    update_fact_file(PlayersPath, Player, player(MatchName, PlayerName, UpdatedLetters, PlayerScore)).
