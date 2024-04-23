17c8871d3612eeb9c443fc73f58bee1462aa8e89
raiaiaia <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2024-04-22 23:53:49 -0300

    iniciando a lógica de telas

--- prolog/Interfaces/BoxesMenu.pl
+++ prolog/Interfaces/BoxesMenu.pl
@@ -18,7 +18,7 @@ menu(redimension_screen,
       "                 │  a Line caiba no terminal!  │                ",
       "                 │                             │                ",
       "<-------------------------------------------------------------->",
-      "                 │           > Enter           │                ",
+      "                 │ > Pressione 0 p/ continuar  │                ",
       "                 │                             │                ",
       "                 └─────────────────────────────┘                "]).
 
@@ -76,14 +76,103 @@ menu(continue_game,
      "    │        continuar jogo         │   ",
      "    └───────────────────────────────┘   "]).
 
+
+:- dynamic(current_screen/1).
+:- dynamic(screen/2).
+screen(redimension_screen, []).
+
 update_menu(Action, CurrentMenu, UpdatedMenu) :-
     (menu(Action, NovoMenu) -> UpdatedMenu = NovoMenu ; UpdatedMenu = CurrentMenu).
 
 writeln_menu([]).
 writeln_menu([Line|Rest]) :-
-    writeln(Line),
+    writeln(Line),  
     writeln_menu(Rest).
 
 show_menu(Action) :-
+    clear_screen, 
     update_menu(Action, [], UpdatedMenu),
     writeln_menu(UpdatedMenu).
+
+clear_screen :- (current_prolog_flag(windows, true) -> shell('cls'); shell('clear')).
+
+get_input(Key) :-
+    get_single_char(Code),
+    char_code(Key, Code).
+
+% Define o fluxo de start_menu 
+process_input('0') :- 
+    current_screen(redimension_screen),
+    retract(current_screen(_)),
+    assertz(current_screen(start_menu)),
+    show_menu(start_menu).
+
+process_input('1') :- 
+    current_screen(start_menu),
+    retract(current_screen(_)),
+    assertz(current_screen(new_game)),
+    show_menu(new_game). 
+
+process_input('2') :- 
+    current_screen(start_menu),
+    retract(current_screen(_)),
+    assertz(current_screen(continue_game)),
+    show_menu(continue_game).
+
+process_input('3') :- 
+    current_screen(start_menu),
+    retract(current_screen(_)),
+    assertz(current_screen(rules)),
+    show_menu(rules).
+
+
+process_input('4') :- 
+    current_screen(start_menu),
+    retract(current_screen(_)),
+    assertz(current_screen(rank)),
+    show_menu(rank).
+
+process_input('5') :- 
+    current_screen(start_menu),
+    writeln('Saindo...'), halt.     
+
+% Define o fluxo de new_game 
+process_input('1') :- 
+    current_screen(new_game),
+    retract(current_screen(_)),
+    assertz(current_screen(create_account)),
+    show_menu(create_account). 
+
+process_input('2') :- 
+    current_screen(new_game),
+    retract(current_screen(_)),
+    assertz(current_screen(login)),
+    show_menu(login).
+
+process_input('3') :- 
+    current_screen(new_game),
+    retract(current_screen(_)),
+    assertz(current_screen(start_menu)),
+    show_menu(start_menu).
+
+% Define o fluxo de continue_game
+process_input('1') :- 
+    current_screen(continue_game),
+    retract(current_screen(_)),
+    assertz(current_screen(create_account)),
+    show_menu(create_account). 
+
+process_input('2') :- 
+    current_screen(continue_game),
+    retract(current_screen(_)),
+    assertz(current_screen(continue_game)),
+    show_menu(continue_game).
+
+process_input('3') :- 
+    current_screen(continue_game),
+    retract(current_screen(_)),
+    assertz(current_screen(start_menu)),
+    show_menu(start_menu).
+
+process_input(_) :- 
+    writeln('Opção inválida!').
