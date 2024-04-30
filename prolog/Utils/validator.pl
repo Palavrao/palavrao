3736737f6ed904259a0455b691aa2f957192e714
paulorpn <paulo.ricardo.pereira.nascimento@ccc.ufcg.edu.br>
2024-04-29 21:08:14 -0300

    refactored player_has_letter

--- prolog/Utils/validator.pl
+++ prolog/Utils/validator.pl
@@ -11,9 +11,10 @@ validation(MatchName, InputLine) :-
 
     get_turn_player_name(MatchName, PlayerName),
     get_player_letters(MatchName, PlayerName, PlayerLetters),
-    msort(PlayerLetters, LetrasPlayer),
-    msort(WordLetters, LetrasWord),
-    player_has_letters(LetrasPlayer, LetrasWord),
+    length(WordLetters, WordLength),
+    (IsHorizontal -> N is X + WordLength ; N is Y + WordLength), get_cur_tiles(BoardName, CurTiles),
+    take_up_to(CurTiles, X, Y, N, IsHorizontal, BoardTiles),
+    player_has_letters(WordLetters, PlayerLetters, BoardTiles, ValidLetters, InvalidLetters),
 
     % Lógica de validação da palavra
 
@@ -22,7 +23,7 @@ validation(MatchName, InputLine) :-
     center_tile_validation(BoardName, X, Y, IsHorizontal, WordLetters),
     get_word_list(PortugueseWords),
     word_existence_validation(WordLetters, PortugueseWords),
-    get_cur_tiles(BoardName, CurTiles), get_words(CurTiles, BoardWords),
+    get_words(CurTiles, BoardWords),
     all_words_exist(BoardWords, PortugueseWords).
 
 read_input(InputLine, Info) :-
@@ -52,12 +53,19 @@ read_input(InputLine, Info) :-
 is_alpha(Char) :-
     char_type(Char, alpha).
 
-player_has_letters(_, []) :- !.
-player_has_letters([], _) :- false, !.
-player_has_letters([H|T], [H|Ts]) :-
-    player_has_letters(T, Ts), !.
-player_has_letters([_|T], WordLetters) :-
-    player_has_letters(T, WordLetters).
+player_has_letters([], _, _, [], []).
+player_has_letters([WL|WLs], PlayerLetters, [WL|BTs], ValidLetters, InvalidLetters) :-
+    player_has_letters(WLs, PlayerLetters, BTs, ValidLetters, InvalidLetters), !.
+player_has_letters([WL|WLs], PlayerLetters, [_|BTs], ValidLetters, InvalidLetters) :-
+    (member(WL, PlayerLetters) ->
+        select(WL, PlayerLetters, NewPlayerLetters), !
+    ; member('<', PlayerLetters) ->
+        select('<', PlayerLetters, NewPlayerLetters)),
+    player_has_letters(WLs, NewPlayerLetters, BTs, TempValidLetters, InvalidLetters), 
+    ValidLetters = [WL|TempValidLetters], !.
+player_has_letters([WL|WLs], _, [_|BTs], ValidLetters, InvalidLetters) :-
+    player_has_letters(WLs, _, BTs, ValidLetters, TempInvalidLetters),
+    InvalidLetters = [WL|TempInvalidLetters].
 
 word_fits_in_space(X, Y, WordLetters, IsHorizontal) :-
     length(WordLetters, WordLength), 
@@ -71,11 +79,11 @@ word_tiles_validation(BoardName, WordLetters, X, Y, IsHorizontal) :-
     length(WordLetters, WordLength),
 
     (IsHorizontal -> 
-        To is X + WordLength,
-        take_up_to(CurTiles, X, To, Y, IsHorizontal, SublistTiles)
-    ; To is Y + WordLength,
-        take_up_to(CurTiles, Y, To, X, IsHorizontal, SublistTiles)),
-
+        N is X + WordLength
+    ;   
+        N is Y + WordLength),
+    
+    take_up_to(CurTiles, X, Y, N, IsHorizontal, SublistTiles),
     letter_overlap_validation(WordLetters, SublistTiles),
     not(maplist(is_alpha, SublistTiles)).
 
@@ -86,21 +94,23 @@ letter_overlap_validation([_|T], [X|Y]) :-
     not(is_alpha(X)),
     letter_overlap_validation(T, Y), !.
 
-take_up_to(_, To, To, _, _, []) :- !.
-take_up_to(Matrix, From, To, ListIndex, IsHorizontal, Sublist) :-
-    (IsHorizontal ->
-        nth0(ListIndex, Matrix, List),
-        nth0(From, List, Char)
-    ; nth0(From, Matrix, List),
-        nth0(ListIndex, List, Char)),
+take_up_to(_, X, Y, N, _, []) :- (X =:= N), ! ; (Y =:= N), !.
+take_up_to(Matrix, X, Y, N, IsHorizontal, BoardTiles) :-
+    nth0(Y, Matrix, List),
+    nth0(X, List, Char),
 
-    NewFrom is From + 1,
-    take_up_to(Matrix, NewFrom, To, ListIndex, IsHorizontal, TempSublist),
-    append([Char], TempSublist, Sublist).
+    (IsHorizontal ->
+        NewX is X + 1,
+        take_up_to(Matrix, NewX, Y, N, IsHorizontal, TempBoardTiles)
+    ; 
+        NewY is Y + 1,
+        take_up_to(Matrix, X, NewY, N, IsHorizontal, TempBoardTiles)),
+
+    append([Char], TempBoardTiles, BoardTiles).
 
 center_tile_validation(BoardName, X, Y, IsHorizontal, WordLetters) :-
     get_cur_tiles(BoardName, CurTiles),
-    take_up_to(CurTiles, 7, 8, 7, IsHorizontal, [CenterTile]),
+    take_up_to(CurTiles, 7, 7, 8, IsHorizontal, [CenterTile]),
     length(WordLetters, WordLength),
 
     ((is_alpha(CenterTile), !) ; 
@@ -112,10 +122,14 @@ center_tile_validation(BoardName, X, Y, IsHorizontal, WordLetters) :-
         X =:= 7, Y =< 7, WordLastInd >= 7)).
 
 word_existence_validation(WordLetters, PortugueseWords) :-
-    atomic_list_concat(WordLetters, WordUpper),
-    downcase_atom(WordUpper, Word),
+    atomic_list_concat(WordLetters, WordUpper), downcase_atom(WordUpper, Word),
     member(Word, PortugueseWords).
 
-all_words_exist(BoardWords, PortugueseWords) :-
-    maplist(atom_string, BoardWordsAtom, BoardWords),
-    subset(BoardWordsAtom, PortugueseWords).
+all_words_exist([], _, []).
+all_words_exist([BW|BWs], PortugueseWords, InvalidWords) :-
+    atom_string(AtomBW, BW),
+    (member(AtomBW, PortugueseWords) ->
+        all_words_exist(BWs, PortugueseWords, InvalidWords)
+    ;
+        all_words_exist(BWs, PortugueseWords, TempInvalidWords),
+        InvalidWords = [AtomBW|TempInvalidWords]).
