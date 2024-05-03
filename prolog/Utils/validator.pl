d14572baaf7acadc2aac5a4aa40e34cf728c3553
paulorpn <paulo.ricardo.pereira.nascimento@ccc.ufcg.edu.br>
2024-05-03 18:58:05 -0300

    Validator documentation

--- prolog/Utils/validator.pl
+++ prolog/Utils/validator.pl
@@ -1,6 +1,15 @@
 report(0, Report) :- Report = [false, 0, [], [], []].
 report(1, Points, ValidLetters, InvalidLetters, InvalidWords, Report) :- Report = [true, Points, ValidLetters, InvalidLetters, InvalidWords], !.
 
+
+% Encapsula lógica de validação
+% Recebe: o nome de uma match
+% Recebe: uma string de input
+% Retorna: uma lista com: [True se a coordenada e posicionamento forem válidos,
+%                            A quantidade de pontos obtidos,
+%                            Uma lista de letras usadas pelo jogador,
+%                            Uma lista de letras que o jogador não possui,
+%                            Uma lista de palavra inválidas]
 validation(MatchName, InputLine, Report) :-
     get_match_board_name(MatchName, BoardName),
 
@@ -17,26 +26,25 @@ validation(MatchName, InputLine, Report) :-
         length(WordLetters, WordLength),
 
         (IsHorizontal -> N is X + WordLength ; N is Y + WordLength), get_work_tiles(BoardName, WorkTiles),
-        take_up_to(WorkTiles, X, Y, N, IsHorizontal, BoardTiles),
+        take_up_to(WorkTiles, X, Y, N, IsHorizontal, BoardTiles), not(maplist(special_char, BoardTiles)),
         player_has_letters(WordLetters, PlayerLetters, BoardTiles, ValidLetters, InvalidLetters),
+        not(maplist(is_alpha, BoardTiles)), letter_overlap_validation(WordLetters, BoardTiles), 
         get_points_word(BoardTiles, WordLetters, Points),
 
         % Lógica de validação da palavra
 
         ((word_fits_in_space(X, Y, WordLetters, IsHorizontal)) ->
-            ((word_tiles_validation(WorkTiles, WordLetters, X, Y, IsHorizontal) )->
+            (center_tile_validation(WorkTiles, X, Y, IsHorizontal, WordLetters) ->
 
-                (center_tile_validation(WorkTiles, X, Y, IsHorizontal, WordLetters) ->
+                atomic_list_concat(WordLetters, Word),
+                place_word(X, Y, IsHorizontal, Word, BoardName, NewBoard),
+                get_words(NewBoard, BoardWords),
 
-                    atomic_list_concat(WordLetters, Word),
-                    place_word(X, Y, IsHorizontal, Word, BoardName, NewBoard),
-                    get_words(NewBoard, BoardWords),
-
-                    (all_words_exist(BoardWords, InvalidWords) ->
-                        report(1, Points, ValidLetters, InvalidLetters, InvalidWords, Report),
-                        update_work_tiles(BoardName, NewBoard),!
-                    )
+                (all_words_exist(BoardWords, InvalidWords) ->
+                    report(1, Points, ValidLetters, InvalidLetters, InvalidWords, Report),
+                    update_work_tiles(BoardName, NewBoard),!
                 )
+                
             )
         )
     )
@@ -45,6 +53,11 @@ validation(MatchName, InputLine, Report) :-
 
     report(0, Report).
 
+% Encapsula o tratamento da linha de entrada do jogador
+% Recebe a string de entrada
+% Retorna: uma lista com as informações tratadas: [Coordenada X, Coordenada Y, 
+%                                                   Uma lista com os caracteres da palavra,
+%                                                   True se a direção é horizontal]
 read_input(InputLine, Info) :-
     string_upper(InputLine, InputLineUpper),
     split_string(InputLineUpper, " ", "", Input), length(Input, 3),
@@ -72,20 +85,33 @@ read_input(InputLine, Info) :-
 is_alpha(Char) :-
     char_type(Char, alpha).
 
+% Verifica se o jogador tem as letras da palavra jogada
+% Recebe: A lista com os caracteres da palavra
+% Recebe: As letras do jogador
+% Recebe: Uma sublista do espaço do tabuleiro em que a palavra possivelmente será inserida
+% Retorna: Uma lista de letras válidas utilizadas
+% Retorna: Uma lista de letras inválidas
 player_has_letters([], _, _, [], []).
 player_has_letters([WL|WLs], PlayerLetters, [WL|BTs], ValidLetters, InvalidLetters) :-
     player_has_letters(WLs, PlayerLetters, BTs, ValidLetters, InvalidLetters), !.
 player_has_letters([WL|WLs], PlayerLetters, [_|BTs], ValidLetters, InvalidLetters) :-
     (member(WL, PlayerLetters) ->
-        select(WL, PlayerLetters, NewPlayerLetters), !
+        select(WL, PlayerLetters, NewPlayerLetters),
+        player_has_letters(WLs, NewPlayerLetters, BTs, TempValidLetters, InvalidLetters),
+        ValidLetters = [WL|TempValidLetters], !
     ; member('<', PlayerLetters) ->
-        select('<', PlayerLetters, NewPlayerLetters)),
-    player_has_letters(WLs, NewPlayerLetters, BTs, TempValidLetters, InvalidLetters),
-    ValidLetters = [WL|TempValidLetters], !.
+        select('<', PlayerLetters, NewPlayerLetters),
+        player_has_letters(WLs, NewPlayerLetters, BTs, TempValidLetters, InvalidLetters),
+        ValidLetters = ['<'|TempValidLetters]), !.
 player_has_letters([WL|WLs], _, [_|BTs], ValidLetters, InvalidLetters) :-
     player_has_letters(WLs, _, BTs, ValidLetters, TempInvalidLetters),
     InvalidLetters = [WL|TempInvalidLetters].
 
+% Verifica se o a palavra cabe na coordenada passada
+% Recebe: Coordenada X
+% Recebe: Coordenada Y
+% Recebe: Lista de caracteres da palavra
+% Recebe: Booleano para a direção horizontal
 word_fits_in_space(X, Y, WordLetters, IsHorizontal) :-
     length(WordLetters, WordLength),
 
@@ -93,18 +119,9 @@ word_fits_in_space(X, Y, WordLetters, IsHorizontal) :-
         (X =< 15 - WordLength)
     ; (Y =< 15 - WordLength)).
 
-word_tiles_validation(WorkTiles, WordLetters, X, Y, IsHorizontal) :-
-    length(WordLetters, WordLength),
-
-    (IsHorizontal ->
-        N is X + WordLength
-    ;
-        N is Y + WordLength),
-
-    take_up_to(WorkTiles, X, Y, N, IsHorizontal, SublistTiles),
-    letter_overlap_validation(WordLetters, SublistTiles),
-    not(maplist(is_alpha, SublistTiles)).
-
+% Verifica se a sobreposição da palavra sobre o tabuleiro é válida
+% Recebe: Lista com os caracteres da palavra
+% Recebe: Sublista do espaço do tabuleiro em que a palavra possivelmente será inserida
 letter_overlap_validation([], []) :- !.
 letter_overlap_validation([H|T], [H|Y]) :-
     letter_overlap_validation(T, Y), !.
@@ -112,6 +129,12 @@ letter_overlap_validation([_|T], [X|Y]) :-
     not(is_alpha(X)),
     letter_overlap_validation(T, Y), !.
 
+% Obtém as tiles presentes no board no intervalo especificado
+% Recebe: uma matriz
+% Recebe: coordenadas x e y
+% Recebe: o tamanho do intervalo
+% Recebe: o booleano se informa se é horizontal
+% Retorna: as tiles no intervalo especificado
 take_up_to(_, N, _, N, true, []) :- !.
 take_up_to(_, _, N, N, false, []) :- !.
 take_up_to(Matrix, X, Y, N, IsHorizontal, BoardTiles) :-
@@ -127,6 +150,11 @@ take_up_to(Matrix, X, Y, N, IsHorizontal, BoardTiles) :-
 
     append([Char], TempBoardTiles, BoardTiles).
 
+% Verifica se a primeira jogada utilizou a tile central
+% Recebe: uma matriz
+% Recebe: s coordenadas X e Y
+% Recebe: booleano para a direção horizontal
+% Recebe: lista de caracteres da palavra
 center_tile_validation(WorkTiles, X, Y, IsHorizontal, WordLetters) :-
     take_up_to(WorkTiles, 7, 7, 8, IsHorizontal, [CenterTile]),
     length(WordLetters, WordLength),
@@ -139,6 +167,9 @@ center_tile_validation(WorkTiles, X, Y, IsHorizontal, WordLetters) :-
     ; WordLastInd is Y + WordLength - 1,
         X =:= 7, Y =< 7, WordLastInd >= 7)).
 
+% Verifica se todas as palavras formas no tabuleiro existem na língua portuguesa
+% Recebe: Lista de palavras do tabuleiro
+% Retorna: Lista de palavras inválidas
 all_words_exist([], []).
 all_words_exist([H|T], InvalidWords) :-
     all_words(AW),
@@ -178,5 +209,5 @@ contains_special_chars_helper([_|Rest]) :-
     contains_special_chars_helper(Rest).
 
 special_char(Char) :-
-    member(Char, ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?', '\\', '|',
+    member(Char, ['!', '@', '#', '~', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?', '\\', '|',
                   'á', 'à', 'ã', 'â', 'é', 'è', 'ẽ', 'ê', 'í', 'ì', 'ĩ', 'î', 'ó', 'ò', 'õ', 'ô', 'ú', 'ù', 'ũ', 'û', 'ç']).
