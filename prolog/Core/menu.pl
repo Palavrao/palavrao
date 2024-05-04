770ef26c04cc43136859f1bcef5aac2642534658
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2024-05-04 13:57:29 -0300

    fix: enter duplo nas regras

--- prolog/Core/menu.pl
+++ prolog/Core/menu.pl
@@ -47,8 +47,6 @@ process_input('3') :-
     current_screen(start_menu),
     clear_screen,
     regras,
-    read_line_to_codes(user_input, Codes),
-    string_codes(_, Codes),
     back_to_start_menu.
 
 process_input('4') :-
