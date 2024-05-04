d2bfce2b1534432fbe3cb074e160e78bf767c13a
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2024-05-03 21:54:15 -0300

    doc: adding utils.pl documentation

--- prolog/Utils/utils.pl
+++ prolog/Utils/utils.pl
@@ -1,12 +1,16 @@
 
+% Limpa o terminal
 clear_screen :- (current_prolog_flag(windows, true) -> shell('cls'); shell('clear')).
 
+
+% Cria a pasta do data
 make_data_folder :-
     data_path(Path),
     (exists_directory(Path) ;
     make_directory(Path)).
 
 
+% Inicia a persistência do jogo
 start_persistence :- 
     make_data_folder,
     accs_path(AccsPath),
@@ -19,34 +23,47 @@ start_persistence :-
     make_facts_file(PlayersPath).
 
 
+% Cria os arquivos de fatos para a persistência
+% Recebe: Path do arquivo de fato a ser criado
 make_facts_file(Path) :-
     exists_file(Path) ; 
     open(Path, write, Stream),
     close(Stream).
 
 
+% Salva os predicados num arquivo de fatos
+% Recebe: Path do arquivo que terá os fatos salvados e o predicado que será armazenado
 save_fact_file(Path, Pred) :-
     tell(Path),
     listing(Pred),
     told.
 
 
+% Adiciona um fato na persistencia e no conhecimento do programa
+% Recebe: O path do arquivo que terá os fatos salvados, o novo fato que será adicionado e o predicado que será armazenado
 inc_fact_file(Path, NewFact, Pred) :- 
     assertz(NewFact),
     save_fact_file(Path, Pred).
 
 
+% Atualiza um fato da persistência e do conhecimento do programa
+% Recebe: O path do arquivo que terá o fato atualizado, o fato antigo que será atualizado, o novo fato que será adicionado e o predicado que será armazenado
 update_fact_file(Path, CurrentFact, NewFact, Pred) :-
     retract(CurrentFact),
     assertz(NewFact),
     save_fact_file(Path, Pred).
 
 
+% Deleta um fato da persistência e do conhecimento do programa
+% Recebe: O path do arquivo que terá um fato removido, o fato a ser removido e o predicado que será removido
 del_fact_file(Path, DelFact, Pred) :-
     retract(DelFact),
     save_fact_file(Path, Pred).
 
 
+% Remove um elemento de um "array" e o retorna juntamente com o "array" resultante
+% Recebe: O "array" a ter o elemento removido, a quantidade de elementos faltam para chegar no elemento a ser removido
+% Retorna: O elemento removido e o "array" sem o elemento que foi removido
 remove_one_element([H|T], 0, H, T) :- !.
 remove_one_element([H|T], Quantity, RemovedElement, UpdatedElements) :- 
     NewQuantity is Quantity - 1,
@@ -54,6 +71,9 @@ remove_one_element([H|T], Quantity, RemovedElement, UpdatedElements) :-
     UpdatedElements = [H|OtherElements].
 
 
+% Remove elementos aletórios de um "array", os retorna e o "array" resultante
+% Recebe: O "array" a ter os elementos removidos, a quantidade elementos a serem removidos
+% Retorna: Os elementos removidos do "array" e o "array" sem os elementos removidos
 pop_random_elements([], _, [], []) :- !.
 pop_random_elements(Elements, 0, [], Elements) :- !.
 pop_random_elements(Elements, Quantity, [RemovedElement|RemovedElements], UpdatedElements) :- 
@@ -66,6 +86,9 @@ pop_random_elements(Elements, Quantity, [RemovedElement|RemovedElements], Update
     pop_random_elements(TempUpdatedElements, NewQuantity, RemovedElements, UpdatedElements).
 
 
+% Remove elementos de um "array"
+% Recebe: O "array" a ter elementos removidos e os elementos a serem removidos
+% Retorna: O "array" sem os elementos removidos
 remove_elements([], _, []).
 remove_elements([H|T], ToRemove, Updated) :-
     (   member(H, ToRemove) ->
@@ -76,6 +99,8 @@ remove_elements([H|T], ToRemove, Updated) :-
     ).
 
 
+% Lê o input do usuário sem necessitar do ponto(.) exigido pelo Prolog
+% Retorna: O input do usuário
 no_period_input(Input):- 
     read_line_to_codes(user_input, K),
     string_to_atom(K, AtomInput),
@@ -84,20 +109,28 @@ no_period_input(Input):-
     string_to_atom(UpperInput, Input).
 
 
+% Verifica se o número de start de end menos o de start é maior que 300, geralmente usado para verificar se o turno do jogador passou do tempo limite
+% Recebe: O valor de start e de end a ser comparado com o limite
 too_long(Start,End) :-
     K is End - Start,
-    K > 60.
+    K > 300.
 
 
+% Comparam com o segundo elemento de um par
 compare_second(>, [_,X], [_,Y]) :- X @> Y.
 compare_second(>, [A,X], [B,X]) :- A @> B.
 compare_second(<, [_,X], [_,Y]) :- X @< Y.
 compare_second(=, [A,X], [A,X]).
 
+
+% Faz uma ordenação com base no segundo elemento de um par
 sort_by_second(Pairs, Sorted) :-
     predsort(compare_second, Pairs, Sorted), !.
 
 
+% Retorna os últimos elementos de um "array", sendo o tamanho desse conjunto determinado pelo qtd
+% Recebe: "Array" a ter os últimos elementos retornados e a quantidade de elementos a serem retornados
+% Retorna: Os úlimos "qtd" elementos do "array" passado
 get_last_elements(Elements, Qtd, LastElements) :- length(Elements, Len), Len =:= Qtd, LastElements = Elements.
 get_last_elements([_|T], Qtd, LastElements) :- get_last_elements(T, Qtd, LastElements), !.
 
@@ -138,6 +171,7 @@ regras :-
     print_short_rules.
 
 
+% Escreve as regras de maneira resumida no terminal
 print_short_rules:- 
     ansi_format([bold, fg(green)],' \nPontuações especiais:\n\n', []),
 
