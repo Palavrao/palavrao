82ffab3b8596e4802cbefd64a0d94a8dadd57119
raiaiaia <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2024-05-04 16:09:18 -0300

    fixing bugs

--- prolog/run_project.sh
+++ prolog/run_project.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-swipl -s boot.pl 2>/dev/null -g boot -t halt.
+if [ ! -d "data" ]; then
+    swipl -s boot.pl 2>/dev/null -g boot -t halt.
+fi
+
 swipl -s main.pl 2>/dev/null -g main -t halt.
