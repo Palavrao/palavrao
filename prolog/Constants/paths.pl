154b6bfb7305377b6d03ae8a608fbd258d068f3d
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2024-05-03 22:02:39 -0300

    doc: adding paths.pl documentation

--- prolog/Constants/paths.pl
+++ prolog/Constants/paths.pl
@@ -1,6 +1,19 @@
+
+% Path para a pasta de persistência
 data_path('data').
+
+
+% Path para o arquivo de persistência das contas
 accs_path('./data/accounts.pl').
+
+
+% Path para o arquivo de persistência das partidas
 matches_path('./data/matches.pl').
+
+
+% Path para o arquivo de persistência dos players
 players_path('./data/players.pl').
+
+
+% Path para o arquivo de persistência dos tabuleiros
 boards_path('./data/boards.pl').
-words_path('../haskell/palavras/br-sem-acentos.txt').
