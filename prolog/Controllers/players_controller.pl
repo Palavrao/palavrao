b673e2852dfb5a38ecba62ae9dd6443cb37bff8a
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2024-05-03 21:10:12 -0300

    doc: adding players_controller.pl documentation

--- prolog/Controllers/players_controller.pl
+++ prolog/Controllers/players_controller.pl
@@ -1,27 +1,43 @@
+
+% Verifica se o player com o nome e partida passados existe
+% Recebe: Nome da partida e do player
 player_exists(MatchName, PlayerName) :-
     current_predicate(player/4),
     player(MatchName, PlayerName, _, _), !.
 
 
-create_player(MatchName , PlayerName) :- 
+% Cria um player que será reconhecido por seu nome e o nome da partida em que está, e salva na persistência
+% Recebe: Nome da partida e do player
+create_player(MatchName, PlayerName) :- 
     \+ player_exists(MatchName, PlayerName),
     players_path(PlayersPath),
     inc_fact_file(PlayersPath, player(MatchName, PlayerName, [], 0), player), !.
 
 
+% Retorna o átomo do player no formato player(NomeDaPartida, NomeDoPlayer, [LetrasDoPlayer], ScoreDoPlayer)
+% Recebe: Nome da partida e do player
+% Retorna: Átomo do player
 get_player(MatchName, PlayerName, Player) :- 
     player(MatchName, PlayerName, PlayerLetters, PlayerScore),
     Player = player(MatchName, PlayerName, PlayerLetters, PlayerScore).
 
 
+% Retorna as letras do player selecionado
+% Recebe: Nome da partida e do player
+% Retorna: Array de letras do player
 get_player_letters(MatchName, PlayerName, PlayerLetters) :- 
     player(MatchName, PlayerName, PlayerLetters, _).
 
 
+% Retorna o score do player selecionado
+% Recebe: Nome da partida e do player
+% Retorna: Score do player
 get_player_score(MatchName, PlayerName, PlayerScore) :-
     player(MatchName,PlayerName, _, PlayerScore).
 
 
+% Aumenta o score do player selecionado
+% Recebe: Nome da partida e do player e o score a ser adicionado ao player
 inc_score(MatchName, PlayerName, IncScore) :- 
     players_path(PlayersPath),
 
@@ -33,6 +49,8 @@ inc_score(MatchName, PlayerName, IncScore) :-
     update_fact_file(PlayersPath, Player, player(MatchName, PlayerName, PlayerLetters, NewScore), player).
 
 
+% Atualiza as letras de um player
+% Recebe: Nome da partida e do player e as novas letras que serão armazenadas pelo player
 update_letters(MatchName, PlayerName, NewLetters) :- 
     players_path(PlayersPath),
 
@@ -42,6 +60,8 @@ update_letters(MatchName, PlayerName, NewLetters) :-
     update_fact_file(PlayersPath, Player, player(MatchName, PlayerName, NewLetters, PlayerScore), player).
 
 
+% Adicona novas letras nas letras armazenadas pelo jogador
+% Recebe: Nome da partida e do player e as letras que serão adicionadas nas letras armazenadas pelo jogador
 add_letters(MatchName, PlayerName, NewLetters) :- 
     players_path(PlayersPath),
 
